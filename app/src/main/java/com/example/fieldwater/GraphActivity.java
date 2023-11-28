bce2d9b7343c5d4d7571f91b2dd02f8b3c5adea9
PAPPURAJ <pappuraj.duet@gmail.com>
2023-11-28 21:15:47 +0600

    Final

---
+++ app/src/main/java/com/example/fieldwater/GraphActivity.java
@@ -0,0 +1,95 @@
+package com.example.fieldwater;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+public class GraphActivity extends AppCompatActivity {
+
+    private GraphView graphView;
+    private DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference();
+
+    LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
+    int i=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+        graphView=findViewById(R.id.idGraphView);
+
+        // Set up the graph
+        graphView.setTitle("My Graph View");
+        graphView.setTitleColor(R.color.black);
+        graphView.setTitleTextSize(18);
+        graphView.addSeries(series);
+        graphView.getViewport().setScalable(true);
+        graphView.getViewport().setScalableY(true);
+        graphView.getViewport().setYAxisBoundsManual(true);
+        graphView.getViewport().setMinX(0);
+        graphView.getViewport().setMaxX(4); // Adjust this value as needed
+        graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
+            @Override
+            public String formatLabel(double value, boolean isValueX) {
+                if (!isValueX) {
+                    return String.format("%.0" +
+                            "fcm", value);
+                }
+                return super.formatLabel(value, isValueX);
+            }
+        });
+
+        loadData();
+    }
+
+    void loadData(){
+        databaseReference.child("WaterLevel").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                try{
+                    series.appendData(new DataPoint(i++, Double.parseDouble(snapshot.getValue(String.class))),true,i);
+                    graphView.addSeries(series);
+                    series.notify();
+                    graphView.notify();
+                    graphView.invalidate();
+                    graphView.computeScroll();
+                }catch (Exception e){
+                    Log.e("GraphActivity", "Error updating graph", e);
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/fieldwater/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.fieldwater;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private TextView textView;
+    private DatabaseReference databaseReference=FirebaseDatabase.getInstance().getReference();
+    private int value=0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressBar=findViewById(R.id.waterPercentPb);
+        textView=findViewById(R.id.waterPercentTv);
+        loadFirebase();
+
+    }
+
+
+    void loadFirebase(){
+        databaseReference.child("WaterLevel").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                try{
+                    value=Integer.parseInt(snapshot.getValue(String.class));
+                    progressBar.setProgress(value);
+                    textView.setText(value+"%");
+                }catch (Exception e){
+
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public void gotoGraphClick(View view) {
+        startActivity(new Intent(this,GraphActivity.class));
+    }
+}
